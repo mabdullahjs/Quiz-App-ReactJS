be27b6945346a75cc9555e58e0ed5b1e530fa591
Abdullah <mabdullah2037@gmail.com>
2023-02-02 23:36:56 +0500

    question changin functionality done

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -6,13 +6,25 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
 
 import { Box } from '@mui/system';
 
 function Quiz({ question }) {
+  // all question state
   let [index, setIndex] = useState(0);
+
+  // form validation state
+  let [radio, setRadio] = useState("");
+  // let [option01 , setOption01] = useState("");
+  // let [option02 , setOption02] = useState("");
+  // let [option03 , setOption03] = useState("");
+  // let [correctOpt , setCorrectOpt] = useState("");
   console.log(question);
+  console.log(radio);
+
+  const changeQuestion = () => {
+    setIndex(index + 1);
+  }
 
 
   return (
@@ -20,7 +32,7 @@ function Quiz({ question }) {
       {/* {question === undefined ? <h1>undefined</h1> : <Box>{question[index].question}</Box>} */}
       <Box>
         <Typography variant="h3" gutterBottom>
-          what is html ?
+          {question[index].question}
         </Typography>
         <FormControl>
           <RadioGroup
@@ -28,14 +40,14 @@ function Quiz({ question }) {
             defaultValue="female"
             name="radio-buttons-group"
           >
-            <FormControlLabel value="female" control={<Radio />} label="Female" />
-            <FormControlLabel value="male" control={<Radio />} label="Male" />
-            <FormControlLabel value="other" control={<Radio />} label="Other" />
+            <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option01} control={<Radio />} label={question[index].option01} />
+            <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option02} control={<Radio />} label={question[index].option02} />
+            <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option03} control={<Radio />} label={question[index].option03} />
           </RadioGroup>
         </FormControl>
       </Box>
       <Box className="pt-5">
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" onClick={changeQuestion}>Submit</Button>
       </Box>
     </Box>
   )
