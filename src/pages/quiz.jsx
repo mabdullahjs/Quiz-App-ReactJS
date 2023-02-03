b8b9d721e994c5acb1ea299c8a9c37cb7f2db402
Abdullah <mabdullah2037@gmail.com>
2023-02-03 15:16:19 +0500

    quiz done except marks functionality

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -6,7 +6,6 @@ import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-
 import { Box } from '@mui/system';
 
 function Quiz({ question }) {
@@ -15,40 +14,38 @@ function Quiz({ question }) {
 
   // form validation state
   let [radio, setRadio] = useState("");
-  // let [option01 , setOption01] = useState("");
-  // let [option02 , setOption02] = useState("");
-  // let [option03 , setOption03] = useState("");
-  // let [correctOpt , setCorrectOpt] = useState("");
   console.log(question);
   console.log(radio);
 
   const changeQuestion = () => {
-    setIndex(index + 1);
+    setIndex(index+1);
+    console.log(radio);
   }
-
 
   return (
     <Box className="p-5">
-      {/* {question === undefined ? <h1>undefined</h1> : <Box>{question[index].question}</Box>} */}
-      <Box>
-        <Typography variant="h3" gutterBottom>
-          {question[index].question}
-        </Typography>
-        <FormControl>
-          <RadioGroup
-            aria-labelledby="demo-radio-buttons-group-label"
-            defaultValue="female"
-            name="radio-buttons-group"
-          >
-            <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option01} control={<Radio />} label={question[index].option01} />
-            <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option02} control={<Radio />} label={question[index].option02} />
-            <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option03} control={<Radio />} label={question[index].option03} />
-          </RadioGroup>
-        </FormControl>
-      </Box>
-      <Box className="pt-5">
-        <Button variant="contained" onClick={changeQuestion}>Submit</Button>
-      </Box>
+      {question === undefined ? <Typography variant="h3">No Question Available</Typography> : index < question.length ? <Box>
+        <Box>
+          <Typography variant="h3" gutterBottom>
+            {question[index].question}
+            {/* {index < question.length ? question[index].question : <p>hello</p>} */}
+          </Typography>
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option01} control={<Radio />} label={question[index].option01} />
+              <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option02} control={<Radio />} label={question[index].option02} />
+              <FormControlLabel onChange={(e) => setRadio(e.target.value)} value={question[index].option03} control={<Radio />} label={question[index].option03} />
+            </RadioGroup>
+          </FormControl>
+        </Box>
+        <Box className="pt-5">
+          <Button variant="contained" onClick={changeQuestion}>Submit</Button>
+        </Box>
+      </Box> : <Box>dafa ho</Box>}
     </Box>
   )
 }
